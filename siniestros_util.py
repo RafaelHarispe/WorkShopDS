1dc56fddc5f542b6e2814090ea6e791bd2a3ee0f
RafaelHarispe <rafael.harispe@gmail.com>
2019-11-17 18:01:03 -0300

    Funciones, Stack y UnStack

--- siniestros_util.py
+++ siniestros_util.py
@@ -1,3 +1,13 @@
 import pandas as pd
+import getpass
+import json
+
+
+with open('FieldsMapper.json', 'r') as f:
+    fieldsemantic = json.load(f)
+
 def data_frame_siniestros():
-	return pd.read_excel('data\Siniestros.xlsx', sheet_name = 'Clientes', index_col = 'Cliente')
+	#password = getpass.getpass()
+	df = pd.read_excel('data\Siniestros.xlsx', sheet_name = 'Clientes', index_col = 'Cliente')
+	df.rename(columns = fieldsemantic, inplace = True)	
+	return df
