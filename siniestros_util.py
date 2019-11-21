09e5e469779948760c17fd2814ade0636313f613
RafaelHarispe <rafael.harispe@gmail.com>
2019-11-21 13:18:39 -0300

    Objetivos y proximos pasos

--- siniestros_util.py
+++ siniestros_util.py
@@ -7,7 +7,6 @@ with open('FieldsMapper.json', 'r') as f:
     fieldsemantic = json.load(f)
 
 def data_frame_siniestros():
-	#password = getpass.getpass()
 	df = pd.read_excel('data\Siniestros.xlsx', sheet_name = 'Clientes', index_col = 'Cliente')
 	df.rename(columns = fieldsemantic, inplace = True)	
 	return df
